cd77e5e0c18522ef276ea649dbc21f02ed631603
nithin <programming965@gmail.com>
2021-07-29 12:19:06 +0530

    feat: Persisting user after authentication

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import Chat from "./components/Chat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useState } from "react";
 import Login from "./components/Login";
+import EmptyChat from "./components/EmptyChat";
 import { useStateValue } from "./StateProvider";
+import { useEffect } from "react";
+import { auth } from "./config/firebase";
+import { actionTypes } from "./reducer";
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: user,
+        });
+      }
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -18,11 +33,17 @@ function App() {
           <Router>
             <Sidebar />
             <Switch>
-              <Route path="/rooms/:roomId">
+              <Route path="/rooms/:roomId" exact>
                 <Chat />
               </Route>
-              <Route path="/">
+              <Route path="/" exact>
                 <Chat />
+              </Route>
+              {/* <Route path="/profile" exact>
+
+              </Route> */}
+              <Route path="/emptyChat" exact>
+                <EmptyChat />
               </Route>
             </Switch>
           </Router>
