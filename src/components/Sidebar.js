8fa49873806667b9312355f6c0fed7177c83034d
nithin <programming965@gmail.com>
2021-09-17 22:12:45 +0530

    fix: rooms not showing after signin in if not refreshed and interchanged visibility icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import "./componetsStyles/Sidebar.css";
-import { Avatar, IconButton } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import './componetsStyles/Sidebar.css';
+import { Avatar, IconButton } from '@material-ui/core';
 import {
   Chat,
   DonutLarge,
@@ -9,68 +9,69 @@ import {
   ExitToAppOutlined,
   VisibilityOff,
   Visibility,
-} from "@material-ui/icons";
-import SidebarChat from "./SidebarChat";
+} from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
-import db, { auth } from "../config/firebase";
-import { useStateValue } from "../StateProvider";
-import { useHistory, Link } from "react-router-dom";
-import { actionTypes } from "../reducer";
+import db, { auth } from '../config/firebase';
+import { useStateValue } from '../StateProvider';
+import { useHistory, Link } from 'react-router-dom';
+import { actionTypes } from '../reducer';
 
 const Sidebar = () => {
   const [rooms, setRooms] = useState([]);
+  const [privateRooms, setPrivateRooms] = useState([]);
   const [{ user }, dispatch] = useStateValue();
-  const [addrtype, setAddrtype] = useState(["Normal", "Admin"]);
+  const [addrtype, setAddrtype] = useState(['Normal', 'Admin']);
   const Add = addrtype.map((Add) => Add);
   let history = useHistory();
-  const [roomtype, setRoomtype] = useState("");
-  const [roomName, setRoomName] = useState("");
-  const [roomUsers, setRoomUsers] = useState("");
+  const [roomtype, setRoomtype] = useState('');
+  const [roomName, setRoomName] = useState('');
+  const [roomUsers, setRoomUsers] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const [showHidden, setShowHidden] = useState(false);
   console.log(roomtype);
 
   const reset = () => {
     setIsOpen(!isOpen);
-    setRoomName("");
-    setRoomUsers("");
+    setRoomName('');
+    setRoomUsers('');
   };
 
   const handleCreateRoom = async () => {
     console.log(roomUsers);
-    const newArr = roomUsers.split(",");
+    const newArr = roomUsers.split(',');
     console.log(newArr);
     const roomUserIds = newArr.map((user) => {
-      return user.replace("http://localhost:3000/profile/", "");
+      return user.replace('http://localhost:3000/profile/', '');
     });
-    if (roomtype === "Private") {
+    if (roomtype === 'Private') {
       if (!roomName || !roomUsers) {
-        alert("Please fill all fields");
+        alert('Please fill all fields');
       } else {
         await db
-          .collection("rooms")
+          .collection('rooms')
           .add({
             name: roomName,
             type: roomtype,
             users: roomUserIds,
           })
           .then(() => {
-            console.log("Private room added successfully");
+            console.log('Private room added successfully');
           })
           .catch((err) => alert(err.message));
       }
     } else {
       if (!roomName) {
-        alert("Please fill all fields");
+        alert('Please fill all fields');
       } else {
         await db
-          .collection("rooms")
+          .collection('rooms')
           .add({
             name: roomName,
             type: roomtype,
           })
           .then(() => {
-            console.log("Public room added successfully");
+            console.log('Public room added successfully');
           })
           .catch((err) => alert(err.message));
       }
@@ -79,43 +80,20 @@ const Sidebar = () => {
     reset();
   };
 
-  const handleShowPrivateRooms = async () => {
+  const handleShowPrivateRooms = () => {
     setShowHidden(!showHidden);
-    return showHidden
-      ? await db
-          .collection("rooms")
-          .where("users", "array-contains", user.uid)
-          .onSnapshot((snapshot) =>
-            setRooms(
-              snapshot.docs.map((doc) => ({
-                id: doc.id,
-                data: doc.data(),
-              }))
-            )
-          )
-      : await db
-          .collection("rooms")
-          .where("type", "==", "Public")
-          .onSnapshot((snapshot) =>
-            setRooms(
-              snapshot.docs.map((doc) => ({
-                id: doc.id,
-                data: doc.data(),
-              }))
-            )
-          );
   };
 
   const handleSignOut = async () => {
     return await auth
       .signOut()
       .then(() => {
-        history.push("/signin");
+        history.push('/signin');
         dispatch({
           type: actionTypes.SET_USER,
           user: null,
         });
-        console.log("Signout Successful");
+        console.log('Signout Successful');
       })
       .catch((err) => {
         return alert(err.message);
@@ -123,23 +101,37 @@ const Sidebar = () => {
   };
 
   useEffect(() => {
-    const unsubscribe = db
-      .collection("rooms")
-      .where("type", "==", "Public")
-      .onSnapshot((snapshot) =>
-        setRooms(
-          snapshot.docs.map((doc) => ({
-            id: doc.id,
-            data: doc.data(),
-          }))
-        )
-      );
+    const savePublicRooms = async () =>
+      await db
+        .collection('rooms')
+        .where('type', '==', 'Public')
+        .onSnapshot((snapshot) =>
+          setRooms(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
 
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-  console.log(showHidden);
+    const savePrivateRooms = async () =>
+      await db
+        .collection('rooms')
+        .where('type', '==', 'Private')
+        .where('users', 'array-contains', user.uid)
+        .onSnapshot((snapshot) =>
+          setPrivateRooms(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+
+    savePublicRooms();
+    savePrivateRooms();
+  }, [user]);
+  console.log(rooms);
 
   return user ? (
     <div className="sidebar">
@@ -149,7 +141,7 @@ const Sidebar = () => {
         </Link>
         <div className="sidebar_headerRight">
           <IconButton onClick={handleShowPrivateRooms}>
-            {showHidden ? <VisibilityOff /> : <Visibility />}
+            {showHidden ? <Visibility /> : <VisibilityOff />}
           </IconButton>
           <IconButton>
             <Chat />
@@ -183,11 +175,12 @@ const Sidebar = () => {
                 value={roomtype}
                 onChange={(e) => setRoomtype(e.target.value)}
               >
+                <option>Select Visibility</option>
                 <option value="Public">Public</option>
                 <option value="Private">Private</option>
               </select>
               <br />
-              {roomtype === "Private" && (
+              {roomtype === 'Private' && (
                 <input
                   type="text"
                   placeholder="Add members by profile urls seperated by comma"
@@ -202,13 +195,33 @@ const Sidebar = () => {
             </form>
           </div>
         )}
-        {rooms.map((room) => (
-          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
-        ))}
+        {/* {!showHidden && <p className="private">Your private rooms</p>}
+        {rooms.length > 0 ? (
+          rooms.map((room) => (
+            <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+          ))
+        ) : (
+          <p className="privateMessage">There are no rooms here</p>
+        )} */}
+        {showHidden && (
+          <p style={{ color: 'green', textAlign: 'center' }}>
+            {' '}
+            Your Private rooms{' '}
+          </p>
+        )}
+        {showHidden
+          ? privateRooms.length > 0 &&
+            privateRooms.map((room) => (
+              <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+            ))
+          : rooms.length > 0 &&
+            rooms.map((room) => (
+              <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+            ))}
       </div>
     </div>
   ) : (
-    history.push("/")
+    history.push('/')
   );
 };
 
